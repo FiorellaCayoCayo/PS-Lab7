9ba5d59d6410a52f6b3c8d858e3f8fdccb0f3468
Fiorella Cayo <83074874+FiorellaCayoCayo@users.noreply.github.com>
2022-06-26 11:30:44 -0500

    or and xor

--- bis_bic.c
+++ bis_bic.c
@@ -1,12 +1,14 @@
 int bis(int x, int m);
 int bic(int x, int m);
+
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 int bool_or(int x, int y) {
-int resultado = __________ ;
+int resultado = bis(x, y);
 return resultado;
 }
+
 /* Calcula x^y usando solo llamadas a las funciones bis y bic */
 int bool_xor(int x, int y) {
-int resultado = __________;
+int resultado = bis(bic(x, y), bic(y, x));
 return resultado;
 }
